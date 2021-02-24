6516a90ef07bb04f3994fa6ef53a4be538513d70
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-24 00:07:27 -0500

    .

--- server.js
+++ server.js
@@ -1,36 +1,10 @@
-const express = require("express");
-const mongoose = require ("mongoose");
-const routes= require ("./routes")
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Define API routes here
-app.use(routes)
-
-//Connet to mongoose
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://boox:12345@cluster0.q43nn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://boox:12345@cluster0.q43nn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 
