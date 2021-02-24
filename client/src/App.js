184568a29881922e9c58b78dce956f81c954c685
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-23 20:34:29 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/navbar/index";
-import SaveBooks from "../src/pages/savedBooksPage";
-import SearchBooks from "../src/pages/searchBooksPage";
+import SaveBooks from "./pages/savedBooksPage";
+import SearchBooks from "./pages/searchBooksPage";
 import "./App.css";
 
 function App() {
--- client/src/components/resultform/index.js
+++ client/src/components/resultform/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "../Grid/index";
 
+
 const styles = {
     save: {
         backgroundColor: "purple",
